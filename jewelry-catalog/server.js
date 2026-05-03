b1b80bff192af652dfbdd292b9c8e1bf8dd1d0e8
Jose Angel <joseangelmarquez857@gmail.com>
2026-05-03 02:55:43 -0600

    nuevo

--- jewelry-catalog/server.js
+++ jewelry-catalog/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor de Nexo Luxury corriendo en el puerto ${PORT}`);
+});
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+
